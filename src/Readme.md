d54bc4250b2cdef30b0744c58d016ab09fa93012
Stephen Way <way.stephen@gmail.com>
2017-11-03 10:33:02 -0700

    Renamed content to country prop

--- src/Readme.md
+++ src/Readme.md
@@ -1,5 +1,45 @@
-Flag example:
+Displays the flag of the United States (US)
 
 ```js
-<Flag content="US" />
+<Flag
+  country="US"
+/>
+```
+
+Component can render different flags by country, a list of all codes  can be found [here](https://github.com/madebybowtie/FlagKit/blob/master/Assets/Flags.md).
+
+```js
+<Flag
+  country="DE"
+/>
+```
+
+It's also possible to make those flags whatever pixel size you want.
+
+Big
+
+```js
+<Flag
+  country="CA"
+  size={84}
+/>
+```
+
+or small
+
+```js
+<Flag
+  country="GB"
+  size={15}
+/>
+```
+
+If needed you can also make the flag interactive with an `onClick` handler, just make sure to change the `role` to `button`
+
+```js
+<Flag
+  country="SE"
+  role="button"
+  onClick={() => {alert("You just clicked on the flag.")}}
+/>
 ```
--- src/flag.js
+++ src/flag.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+/**
+ * Displays an SVG `img` tag with a country flag.
+ * @public
+ * @param       {string} country
+ * @param       {string} role
+ * @param       {number} size
+ * @returns     {object}
+ * @constructor
+ */
 export default function Flag(props) {
-  const { content, role, size, ...otherProps } = props;
-  const flagSrc = require(`./img/SVG/${content}.svg`);
+  const { country, role, size, ...otherProps } = props;
+  const flagSrc = require(`./img/SVG/${country}.svg`);
   return (
     <img
       src={flagSrc}
       role={role}
-      alt={`${content} Flag`}
+      alt={`${country} Flag`}
       height={size}
       width={size}
       {...otherProps}
@@ -17,7 +25,12 @@ export default function Flag(props) {
 }
 
 Flag.propTypes = {
-  content: PropTypes.string,
+  /** The country code for the image path */
+  country: PropTypes.string.isRequired,
+  /** The a11y role type for the `<img />` tag */
+  role: PropTypes.string,
+  /** The size of the image */
+  size: PropTypes.number,
 };
 
 Flag.defaultProps = {
