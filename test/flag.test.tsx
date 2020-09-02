e8e7318d78bb61e6a9f44b3fd40ebde440468b12
Stephen Way <way.stephen@gmail.com>
2020-09-02 16:25:30 -0700

    Removed local image serving

--- test/flag.test.tsx
+++ test/flag.test.tsx
@@ -5,18 +5,18 @@ import { Default as Flag } from '../stories/Flag.stories';
 describe('Flag', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Flag remote role="button" />, div);
+    ReactDOM.render(<Flag role="button" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
   it('renders text when country code is unrecognized', async () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Flag remote country="USSR" size={16} />, div);
+    ReactDOM.render(<Flag country="USSR" size={16} />, div);
     expect(div.innerHTML).toBe('<span>US</span>');
     ReactDOM.unmountComponentAtNode(div);
   });
   it('renders nothing when no country code is supplied', async () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Flag remote country="" alt="Testing" />, div);
+    ReactDOM.render(<Flag country="" alt="Testing" />, div);
     expect(div.innerHTML.length).toBe(0);
     ReactDOM.unmountComponentAtNode(div);
   });
