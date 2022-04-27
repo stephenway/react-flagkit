3e3b7fb0375f931fb6328f07e8f5cfcfad8c00bd
Stephen Way <stephenway@users.noreply.github.com>
2022-04-26 19:32:10 -0700

    Reimport testing

---
+++ test/flag.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Flag from '../src';
+
+describe('Flag', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Flag role="button" />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('renders text when country code is unrecognized', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Flag country="USSR" size={16} />, div);
+    expect(div.innerHTML).toBe('<span>USSR</span>');
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('renders nothing when no country code is supplied', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Flag country="" alt="Testing" />, div);
+    expect(div.innerHTML.length).toBe(0);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
